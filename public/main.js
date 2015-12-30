17ffb894c85c501a6958bb463538d4efdc2cff09
YOUR_GITHUB_USERNAME <zohar.muzafi@gmail.com>
2015-12-29 19:37:46 -0800

    added prevent default after form submit

--- public/main.js
+++ public/main.js
@@ -77,6 +77,7 @@ $(document).ready(function(){
 	});
 
 	$("#contactForm").on("submit", function(event){
+		event.preventDefault();
 		var data = {};
 		data.name= $("#formName").val();
 		data.email= $("#formEmail").val();
